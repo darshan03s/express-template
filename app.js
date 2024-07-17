4bef71304db819a31383d02b89c3c9717bed9993
Darshan S <darshans2727@gmail.com>
2024-07-17 15:12:21 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import indexRoute from './routes/index.route.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.disable('view cache');
+
+app.get('/', indexRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://127.0.0.1:${PORT}`);
+});
